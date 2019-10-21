f106b13fdcf3dabd9da904c67a53c3fab833c788
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2019-10-21 12:54:27 +0530

    Add files via upload

---
+++ interpolation.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name="kevin";
+  String msg="my name is "+name;
+  int x=10;
+  int y=56;
+
+
+  print(msg);
+  print("my name is $name");
+  print("my name is "+name);
+  print("the num of kevin is  "+name.length.toString());
+  print("the num of kevin is ${name.length}");
+  print("the sum of $x and $y is ${x+y}");
+  print("the sub of $x and $y is ${x-y}");
+}
